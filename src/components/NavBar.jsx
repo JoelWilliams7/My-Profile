360cb48ae5a7c5d4e43413bc28f911de2fd7b6cd
Joel Williams <109452946+JoelWilliams7@users.noreply.github.com>
2023-03-22 23:24:04 -0400

    refresh

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { scrollToSection } from "../utils";
-import portfolioProfilePic from "../assets/Portfolio-Profile-Photo.jpg";
 
 import Button from "./Button";
 
